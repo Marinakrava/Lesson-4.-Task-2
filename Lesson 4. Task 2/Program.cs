581de77e4ac3fdad1665b6151a1dbf4e4fb88c12
yxakr <yxakr@LAPTOP-LVHIATSL>
2022-10-10 23:30:32 +0300

    Добавьте файлы проекта.

---
+++ Lesson 4. Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4.Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n= Convert.ToInt32(Console.ReadLine());
+            int sum1 = 0;// счетчик положительных чисел
+            int sum2 = 0;// счетчик отрицательных чисел
+            do// выполнять действие пока...
+            {
+                
+                if (n > 0)
+                    sum1++;             
+                if (n < 0)
+                    sum2++;
+                n = Convert.ToInt32(Console.ReadLine());// последовательность ввода числа. т.е. проверяется условие "пока" не равно 0 и снова вводим
+            }
+            while (n != 0);// ... n не равно 0
+            {
+                if (sum1 < sum2)
+                    Console.WriteLine("Количество отрицательных больше и равно {0}", sum2);
+                if (sum1 > sum2)
+                    Console.WriteLine("Количество положительных больше и равно {0}", sum1);
+            }
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
